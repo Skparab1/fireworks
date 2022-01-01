ebbdfcc01cbdfee5a6a3c253f0bf0c8940496c16
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-01-01 09:44:56 -0800

    added js

--- fireworks.js
+++ fireworks.js
@@ -1,7 +1,12 @@
-const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgb(0,0,0)';
-ctx.fillRect(0,0,width,height);
+      const canvas = document.querySelector('.myCanvas');
+      const width = canvas.width = window.innerWidth;
+      const height = canvas.height = window.innerHeight;
+      const ctx = canvas.getContext('2d');
+
+      ctx.fillStyle = 'rgb(0,0,0)';
+      ctx.fillStyle = 'rgb(255, 0, 0)';
+
+      ctx.beginPath();
+      ctx.moveTo(50, 50);
+      ctx.fill();
